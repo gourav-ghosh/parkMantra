66c77316af9d219d58aaab4f80bdad3eaabbc57b
Gourav Ghosh Rasonix <gourav.ghosh@rasonix.com>
2024-11-10 23:49:11 +0530

    first commit

---
+++ app/src/main/java/com/example/parkmantra/startup/LoginWithPasswordActivity.java
@@ -0,0 +1,16 @@
+package com.example.parkmantra.startup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.parkmantra.R;
+
+public class LoginWithPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_with_password);
+    }
+}
